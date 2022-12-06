a878915080dc4f00c0f68209980f437063a5eb58
Thomas Flensted <thomasflenstedjensen@gmail.com>
2022-12-06 13:18:27 +0100

    bygget funktionalitt som finder klub

--- pickmyclub.py
+++ pickmyclub.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import sqlite3
 app = Flask(__name__)
 
 questions = [
@@ -18,7 +19,24 @@ def survey():
 
 @app.route("/result")
 def result():
-    return render_template("result.html")
+    club = getClub(None)
+    return render_template("result.html", club = club)
+
+def getClub(league):
+
+    conn = sqlite3.connect("clubs.db")
+    cur = conn.cursor()
+    
+    if (league):
+        cur.execute("SELECT name FROM clubs WHERE league = ? ORDER BY RANDOM() LIMIT 1", (league,))
+    else:
+        cur.execute("SELECT name FROM clubs ORDER BY RANDOM() LIMIT 1")
+
+    club = cur.fetchall()
+    conn.close()
+    
+    return club[0][0]
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
